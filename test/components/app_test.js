855eabded5a6511d71c9d8dcc585c2a4078b967e
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-09-06 09:42:00 -0400

    this is a very simple working test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,22 @@
-import { renderComponent , expect } from '../test_helper';
+// what would another dev do to break this component? IT would be chaning the text in the following example
+
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// use describe to group together similar tests
+describe('Test for the App Component', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
+  //use it to test a signle attribute of a target
+  it('shows the correct text', () => {
+
+    // create an instance of App
+    const component = renderComponent(App);
+
+    // Use 'expect' to make an 'assertion' about a target
+    // (here the target is the componenet 'App')
+
+    expect(component).to.contain('React simple starter');
+
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+})
